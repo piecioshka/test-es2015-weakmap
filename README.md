f8595353ddaf41e25c24245f365769bbf1728564
Piotr Kowalski <piecioshka@gmail.com>
2016-09-12 11:46:36 +0200

    Create basic structure

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # test-es2015-weakmap
+
+> :ledger: Testing WeakMap from ECMAScript 2015 spec
+
+## How it works?
+
+1. Create `users` array with simple literal objects
+
+    ```javascript
+    var users = [{ name: 'Peter' }, { name: 'Kate' }];
+    ```
+
+2. Create `weakMap` object - instance of WeakMap constructor
+    
+    ```javascript
+    var weakMap = new WeakMap();
+    ```
+
+3. Set two pairs where keys are objects from users
+
+    ```javascript
+    weakMap.set(users[0], 'some info 1');
+    weakMap.set(users[1], 'some info 2');
+    ```
+
+4. Remove first element from `users` array
+    
+    ```javascript
+    users.splice(0, 1);
+    ```
+
+5. Wait some time for Garbage Collector, which reduce `weakMap` collection
+
+    ![](./screenshots/gc-activate.png)
+
+## History
+
+![](./screenshots/screenshot.png)
+
+## License
+
+[The MIT License](http://piecioshka.mit-license.org) @ 2016
---
+++ index.js
@@ -0,0 +1,30 @@
+var users = [{ name: 'Peter' }, { name: 'Kate' }];
+console.info('[+] Create users =', JSON.stringify(users));
+
+var weakMap = new WeakMap();
+console.info('[+] Create weakMap =', JSON.stringify(weakMap));
+
+weakMap.set(users[0], 'some info 1');
+console.info('[+] Set message for users[0] =', 'some info 1');
+
+weakMap.set(users[1], 'some info 2');
+console.info('[+] Set message for users[1] =', 'some info 2');
+
+console.log('[*] Display message for users[0]: "%s"', weakMap.get(users[0])); // 'some info 1'
+
+users.splice(0, 1);
+console.warn('[-] Remove first object from "users" array');
+
+console.log('[*] Display message for users[0]: "%s"', weakMap.get(users[0])); // 'some info 2'
+
+const GC_TIMEOUT = 15 * 1000;
+const GC_CHECKER_INTERVAL = 100;
+
+setTimeout(function () {
+    setInterval(function () {
+        console.log('%c Display weakMap collection', 'color: green', weakMap);
+    }, GC_CHECKER_INTERVAL);
+}, GC_TIMEOUT);
+
+console.info('%c Wait "%d" time for GC', 'color: blue', GC_TIMEOUT);
+console.info('%c After that time execute checking on each "%d"', 'color: blue', GC_CHECKER_INTERVAL);
